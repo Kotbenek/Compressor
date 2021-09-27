3ff45da8cb27de3671ebf2edcdf0cb487ff5fcbd
Kotbenek <dominik.piatkowski@protonmail.com>
2021-09-27 14:19:22 +0200

    Cleaned up test script

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 TESTS_DIR="tests"
+
 PASS="[$(tput setaf 2)PASS$(tput sgr0)]"
 FAIL="[$(tput setaf 1)FAIL$(tput sgr0)]"
 RUNNING="[    ]"
 
-# Printing functions
+TESTS=0
+TESTS_PASSED=0
 
+# Printing functions
 print_test_init()
 {
     echo -n "$RUNNING $1"
@@ -25,10 +28,11 @@ print_test_fail()
 }
 
 # Testing functions
-
 assert_equal()
 {
+    ((TESTS=TESTS+1))
     if [ "$1" == "$2" ]; then
+        ((TESTS_PASSED=TESTS_PASSED+1))
         print_test_pass
     else
         print_test_fail
@@ -38,79 +42,57 @@ assert_equal()
     fi
 }
 
+test_compress_decompress_huffman()
+{
+    ARG1="${FILE}"
+    ARG2="${TESTS_DIR}/$(basename ${FILE}).huffman"
+    ARG3="${TESTS_DIR}/$(basename ${FILE})"
+    
+    ./bin/compressor "-hc" "${ARG1}" "${ARG2}"
+    ./bin/compressor "-hd" "${ARG2}" "${ARG3}"
+    
+    assert_equal "$(sha256sum ${ARG1} | cut -d ' ' -f 1)" "$(sha256sum ${ARG3} | cut -d ' ' -f 1)"
+}
+
 # Prepare test directory
 if [ -d "$TESTS_DIR" ]; then rm -rf "$TESTS_DIR"; fi
 mkdir "$TESTS_DIR"
 
 # Run tests
-
-print_test_init "Huffman test - text file  -  build_and_test.sh"
+print_test_init "Huffman - text file  -  build_and_test.sh"
 FILE="build_and_test.sh"
-ARG1="${FILE}"
-ARG2="${TESTS_DIR}/$(basename ${FILE}).huffman"
-ARG3="${TESTS_DIR}/$(basename ${FILE})"
-./bin/compressor "-hc" "${ARG1}" "${ARG2}"
-./bin/compressor "-hd" "${ARG2}" "${ARG3}"
-assert_equal "$(sha256sum ${ARG1} | cut -d ' ' -f 1)" "$(sha256sum ${ARG3} | cut -d ' ' -f 1)"
+test_compress_decompress_huffman
 
-print_test_init "Huffman test - text file  -  test.sh"
+print_test_init "Huffman - text file  -  test.sh"
 FILE="test.sh"
-ARG1="${FILE}"
-ARG2="${TESTS_DIR}/$(basename ${FILE}).huffman"
-ARG3="${TESTS_DIR}/$(basename ${FILE})"
-./bin/compressor "-hc" "${ARG1}" "${ARG2}"
-./bin/compressor "-hd" "${ARG2}" "${ARG3}"
-assert_equal "$(sha256sum ${ARG1} | cut -d ' ' -f 1)" "$(sha256sum ${ARG3} | cut -d ' ' -f 1)"
+test_compress_decompress_huffman
 
-print_test_init "Huffman test - text file  -  makefile"
+print_test_init "Huffman - text file  -  makefile"
 FILE="makefile"
-ARG1="${FILE}"
-ARG2="${TESTS_DIR}/$(basename ${FILE}).huffman"
-ARG3="${TESTS_DIR}/$(basename ${FILE})"
-./bin/compressor "-hc" "${ARG1}" "${ARG2}"
-./bin/compressor "-hd" "${ARG2}" "${ARG3}"
-assert_equal "$(sha256sum ${ARG1} | cut -d ' ' -f 1)" "$(sha256sum ${ARG3} | cut -d ' ' -f 1)"
+test_compress_decompress_huffman
 
-print_test_init "Huffman test - binary file - zero_4K"
+print_test_init "Huffman - binary file - zero_4K"
 FILE="${TESTS_DIR}/zero_4K"
 head -c 4KB /dev/zero > ${FILE}
-ARG1="${FILE}"
-ARG2="${TESTS_DIR}/$(basename ${FILE}).huffman"
-ARG3="${TESTS_DIR}/$(basename ${FILE})"
-./bin/compressor "-hc" "${ARG1}" "${ARG2}"
-./bin/compressor "-hd" "${ARG2}" "${ARG3}"
-assert_equal "$(sha256sum ${ARG1} | cut -d ' ' -f 1)" "$(sha256sum ${ARG3} | cut -d ' ' -f 1)"
+test_compress_decompress_huffman
 
-print_test_init "Huffman test - binary file - random_4K"
+print_test_init "Huffman - binary file - random_4K"
 FILE="${TESTS_DIR}/random_4K"
 head -c 4KB /dev/urandom > ${FILE}
-ARG1="${FILE}"
-ARG2="${TESTS_DIR}/$(basename ${FILE}).huffman"
-ARG3="${TESTS_DIR}/$(basename ${FILE})"
-./bin/compressor "-hc" "${ARG1}" "${ARG2}"
-./bin/compressor "-hd" "${ARG2}" "${ARG3}"
-assert_equal "$(sha256sum ${ARG1} | cut -d ' ' -f 1)" "$(sha256sum ${ARG3} | cut -d ' ' -f 1)"
+test_compress_decompress_huffman
 
-print_test_init "Huffman test - binary file - zero_256K"
+print_test_init "Huffman - binary file - zero_256K"
 FILE="${TESTS_DIR}/zero_256K"
 head -c 256KB /dev/zero > ${FILE}
-ARG1="${FILE}"
-ARG2="${TESTS_DIR}/$(basename ${FILE}).huffman"
-ARG3="${TESTS_DIR}/$(basename ${FILE})"
-./bin/compressor "-hc" "${ARG1}" "${ARG2}"
-./bin/compressor "-hd" "${ARG2}" "${ARG3}"
-assert_equal "$(sha256sum ${ARG1} | cut -d ' ' -f 1)" "$(sha256sum ${ARG3} | cut -d ' ' -f 1)"
+test_compress_decompress_huffman
 
-print_test_init "Huffman test - binary file - random_256K"
+print_test_init "Huffman - binary file - random_256K"
 FILE="${TESTS_DIR}/random_256K"
 head -c 256KB /dev/urandom > ${FILE}
-ARG1="${FILE}"
-ARG2="${TESTS_DIR}/$(basename ${FILE}).huffman"
-ARG3="${TESTS_DIR}/$(basename ${FILE})"
-./bin/compressor "-hc" "${ARG1}" "${ARG2}"
-./bin/compressor "-hd" "${ARG2}" "${ARG3}"
-assert_equal "$(sha256sum ${ARG1} | cut -d ' ' -f 1)" "$(sha256sum ${ARG3} | cut -d ' ' -f 1)"
+test_compress_decompress_huffman
 
 # Remove test directory
 rm -rf "$TESTS_DIR"
 
+# Display test results
+echo "$TESTS_PASSED/$TESTS tests passed"
